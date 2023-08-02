b3810dc402029a29e42c81b2eb7c62a758141735
polarizz <polarizz39@gmail.com>
2023-08-01 17:21:21 -0700

    Initial screening page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,77 @@
 import Image from 'next/image'
 import './page.css'
+import './fonts.css'
 
 export default function Home() {
   return (
-  <div>
-      <div className="nav">
-        <p>logo</p>
-        <p>hamburger</p>
-      </div>
-      <div className = "idekWhatthisis">
-        <p>Screening</p>
-        <p>About</p>
-      </div>
-      <div  className='main'>
-        <div className = "card">
-          <div>
-            <div className="bars"><p>__________________</p><p>__________________</p><p>__________________</p></div>
-            <div><p className="prompt">What are the symptoms in the affected area?</p></div>
-            <div><p className='subtext'>Choose one or more options</p></div>
-            <div className='buttons'>
-              <button>18-24</button>
-              <button>Male</button>
-              <button>Orange</button>
-              <button>AB</button>
-              <button>120lb</button>
-              <button>{`5\'7\"`}</button>
-            </div>
+    <body>
+      <div className="flex flex-col m-4">
+        <div className="flex justify-between items-center mb-4">
+          <img className="w-16" src="/poppyFull.svg" alt="Poppy"></img>
+
+          <div className="flex flex-col justify-between h-3.5 w-5">
+            <div className="h-[2.5px] bg-black w-full"></div>
+            <div className="h-[2.5px] bg-black w-full"></div>
+            <div className="h-[2.5px] bg-black w-full"></div>
           </div>
-          <div className='bruh'><button>Back</button><button>Next</button></div>
         </div>
-        
+
+        <div className="bg-gray-300 h-px"></div>
+
+        <div className="flex justify-between my-4">
+            <div></div>
+            <div className="space-x-10 flex">
+                <p>Screening</p>
+                <p>About</p>
+            </div>
+        </div>
+
+        <div className="flex justify-between p-2 bg-[#FFFAF0] rounded-2xl border-2 border-black overflow-hidden h-[85vh]">
+            <div className="flex-grow"></div>
+            <div className="flex flex-col justify-between">
+                <div className="flex-grow"></div>
+
+                <h1 className="text-center mb-3  px-10">What are the symptoms in the affected area?</h1>
+                <caption className="text-center mb-10 px-10">Choose one or more options</caption>
+
+                <div className="flex flex-wrap justify-center items-center">
+                  <div className="px-5 py-3 m-1 bg-[#FFFAF0] rounded-full border-2 border-black">
+                    <p className="text-center">18-24</p>
+                  </div>
+
+                  <div className="px-5 py-3 m-1 bg-[#FFFAF0] rounded-full border-2 border-black">
+                    <p className="text-center">Male</p>
+                  </div>
+
+                  <div className="px-5 py-3 m-1 bg-[#FFFAF0] rounded-full border-2 border-black">
+                    <p className="text-center">AB+</p>
+                  </div>
+
+                  <div className="px-5 py-3 m-1 bg-[#FFFAF0] rounded-full border-2 border-black">
+                    <p className="text-center">120 lbs</p>
+                  </div>
+
+                  <div className="px-5 py-3 m-1 bg-[#FFFAF0] rounded-full border-2 border-black">
+                    <p className="text-center">5'7"</p>
+                  </div>
+                </div>
+
+                <div className="flex-grow"></div>
+
+                <div className="flex w-full">
+                  <div className="flex-grow justify-between px-5 py-3 m-1 bg-[#FFFAF0] rounded-[10px] border-2 border-black">
+                    <p className="text-center">Back</p>
+                  </div>
+
+                  <div className="flex-grow justify-between px-5 py-3 m-1 bg-[#000] rounded-[10px] border-2 border-black">
+                    <p className="text-center invert">Next</p>
+                  </div>
+                </div>
+            </div>
+            <div className="flex-grow"></div>
+        </div>
+
       </div>
-  </div>
+    </body>
   )
 }
