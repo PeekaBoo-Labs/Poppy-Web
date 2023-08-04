fe7549c4ab4a239634388fe7e483719fa79e2f36
polarizz <polarizz39@gmail.com>
2023-08-04 21:25:42 +0900

    .

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,61 @@
 import Image from 'next/image'
+import './NavBar.css'
 
-export default function NavBar(){
+export default function NavBar() {
     return (
         <div className="flex justify-between items-center mb-4">
-          <Image width={4} height={4} className="w-16" src="/poppyFull.svg" alt="Poppy"></Image>
+            <Image width={4} height={4} className="w-16" src="/poppyFull.svg" alt="Poppy" />
 
-          <div className="flex flex-col justify-between h-3.5 w-5">
-            <div className="h-[2.5px] bg-black w-full"></div>
-            <div className="h-[2.5px] bg-black w-full"></div>
-            <div className="h-[2.5px] bg-black w-full"></div>
-          </div>
+            <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+            <label htmlFor="menu-toggle" className="x">
+                <span></span>
+                <span></span>
+                <span></span>
+            </label>
+
+            <div className="mobile-menu">
+                <div className="column leading">
+                    <h5 className="p-10">Overview</h5>
+                    <a href="/">
+                        <h4 className="p-10">PostBox</h4>
+                    </a>
+                    <a href="/pocket">
+                        <h4 className="p-30">PostBox Pocket</h4>
+                    </a>
+                </div>
+
+                <div className="column leading">
+                    <h5 className="p-10">Resources</h5>
+                    <a href="/resources">
+                        <h4 className="p-10">Localization</h4>
+                    </a>
+                    <a href="/resources">
+                        <h4 className="p-10">Github</h4>
+                    </a>
+                    <a href="/resources">
+                        <h4 className="p-30">Documentation</h4>
+                    </a>
+                </div>
+
+                <div className="column leading">
+                    <h5 className="p-10">Support</h5>
+                    <a href="/faq">
+                        <h4 className="p-10">FAQ</h4>
+                    </a>
+                    <a href="https://discord.gg/5rP7f7jxjf">
+                        <h4 className="p-10">Discord</h4>
+                    </a>
+                    <a href="https://twitter.com/PostBoxTeam">
+                        <h4 className="p-30">Twitter</h4>
+                    </a>
+                </div>
+
+                <a href="/downloads">
+                    <div className="rounded-button-big invert">
+                        <p className="link">Download</p>
+                    </div>
+                </a>
+            </div>
         </div>
     )
 }
