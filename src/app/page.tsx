e0851fe7cc922e747c578ae3972bcc5bf5c0dcab
polarizz <polarizz39@gmail.com>
2023-08-06 10:43:58 +0900

    Cleaned up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,111 +1,75 @@
-import QuestionOption from '@/components/QuestionOption'
-import Caption from '@/components/Caption'
-import { LongButton } from '@/components/Buttons'
-import NavBar from '@/components/NavBar'
+import React from 'react';
+import QuestionOption from '@/components/QuestionOption';
+import Caption from '@/components/Caption';
+import { LongButton } from '@/components/Buttons';
+import NavBar from '@/components/NavBar';
 
-import './page.css'
-import '../styles/fonts.css'
+// You can use dynamic import if ScrollHeader is client-only
+// import dynamic from 'next/dynamic';
+// const ScrollHeader = dynamic(() => import('@/components/ScrollHeader'), { ssr: false });
+
+import './page.css';
+import '../styles/fonts.css';
+
+function QuestionBlock({ bgColor = '#FFFAF0', zIndex = 5 }) {
+  return (
+    <div className={`flex justify-center p-2 bg-[${bgColor}] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[${zIndex}]`}>
+      <div className="flex-grow"></div>
+      <div className="flex flex-col justify-between h-full items-start">
+        <div className="flex-grow"></div>
+        <Caption>Choose all that apply</Caption>
+      </div>
+      <div className="flex-grow"></div>
+    </div>
+  );
+}
 
 export default function Home() {
   return (
-    <body>
-      <div className="flex flex-col m-4">
-        <NavBar/>
-        <div className="bg-gray-300 h-px"></div>
+    <div className="flex flex-col m-4">
+      {/* <ScrollHeader/> */}
 
-        <div className="flex justify-between my-4">
-          <div></div>
-          <div className="space-x-10 flex">
-            <p>Screening</p>
-            <a href="/company/about"><p>About</p></a>
-          </div>
+      <NavBar/>
+      <div className="bg-gray-300 h-px"></div>
+      
+      <div className="flex justify-between my-4">
+        <div></div>
+        <div className="space-x-10 flex">
+          <p>Screening</p>
+          <a href="/company/about"><p>About</p></a>
         </div>
-
-        <div className="flex flex-col space-y-[-190%]  z-[-1]">
-          <div className="flex justify-between p-2 bg-[#FFFAF0] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[5]">
-            <div className="flex-grow"></div>
-            <div className="flex flex-col justify-between">
-              <div className="flex-grow"></div>
-
-              <h1 className="text-center mb-3 px-10">What are the symptoms in the affected area?</h1>
-              <Caption>Choose all that apply</Caption>
-
-              <div className="flex flex-wrap justify-center items-center">
-                {
-                  ["18-4", "Male", "AB+", "120 lbs", "5'7\""].map((text, index) => (
-                    <QuestionOption text={text} selected={index == 0} key={index} />
-                  ))
-                }
-              </div>
-
-              <div className="flex-grow"></div>
-
-              <div className="flex w-full">
-                <LongButton type="secondary">Back</LongButton>
-                <LongButton type="primary">Next</LongButton>
-              </div>
-            </div>
-            <div className="flex-grow"></div>
-          </div>
-
-          <div className="flex justify-center p-2 bg-[#FFFAF0] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[4]">
-            <div className="flex-grow"></div>
-            <div className="flex flex-col justify-between h-full items-start">
-              <div className="flex-grow"></div>
-              <Caption>Choose all that apply</Caption>
-            </div>
-            <div className="flex-grow"></div>
-          </div>
-
-          <div className="flex justify-center p-2 bg-[#FFA701] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[3]">
-            <div className="flex-grow"></div>
-            <div className="flex flex-col justify-between h-full items-start">
-              <div className="flex-grow"></div>
-              <Caption>Choose all that apply</Caption>
-            </div>
-            <div className="flex-grow"></div>
-          </div>
-
-          <div className="flex justify-center p-2 bg-[#FFA701] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[2]">
-            <div className="flex-grow"></div>
-            <div className="flex flex-col justify-between h-full items-start">
-              <div className="flex-grow"></div>
-              <Caption>Choose all that apply</Caption>
-            </div>
-            <div className="flex-grow"></div>
-          </div>
-
-          <div className="flex justify-center p-2 bg-[#FFFAF0] rounded-2xl border-2 border-black overflow-hidden h-[85vh] z-[1]">
-            <div className="flex-grow"></div>
-            <div className="flex flex-col justify-between h-full items-start">
-              <div className="flex-grow"></div>
-              <Caption>Choose all that apply</Caption>
-            </div>
-            <div className="flex-grow"></div>
-          </div>
-        </div>
-
-        <div className="bg-gray-300 h-px my-4"></div>
-
-        <div className="flex w-full">
-                <div className="w-1/2">
-                  <div className="flex flex-col items-start">
-                    <caption>Overview</caption>
-                    <p>Over</p>
-                    <p>Over</p>
-                  </div>
-                </div>
-                <div className="w-1/2">
-                  <div className="flex flex-col items-start">
-                    <caption>Overview</caption>
-                    <p>Over</p>
-                    <p>Over</p>
-                  </div>
-                </div>
-              </div>
-
-
       </div>
-    </body>
-  )
+
+      <div className="flex flex-col space-y-[-190%]">
+        <QuestionBlock bgColor="#FFFAF0" zIndex={5} />
+        <QuestionBlock bgColor="#FFFAF0" zIndex={4} />
+        <QuestionBlock bgColor="#FFA701" zIndex={3} />
+        <QuestionBlock bgColor="#FFA701" zIndex={2} />
+        <QuestionBlock bgColor="#FFFAF0" zIndex={1} />
+      </div>
+
+      <div className="flex justify-center p-2 bg-[#FFFAF0] rounded-2xl border-2 border-black overflow-hidden h-[40vh] mt-4">
+        <div className="flex-grow"></div>
+      </div>
+
+      <div className="bg-gray-300 h-px my-4 mt-10"></div>
+
+      <div className="flex w-full">
+        <div className="w-1/2">
+          <div className="flex flex-col items-start">
+            <caption>Overview</caption>
+            <p>Over</p>
+            <p>Over</p>
+          </div>
+        </div>
+        <div className="w-1/2">
+          <div className="flex flex-col items-start">
+            <caption>Overview</caption>
+            <p>Over</p>
+            <p>Over</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/ScrollHeader.tsx
@@ -0,0 +1,9 @@
+// ScrollHeader.tsx
+
+export default function ScrollHeader() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-16 bg-blue-500 z-50">
+      Your Header Content Here
+    </div>
+  );
+}
