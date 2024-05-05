23d47dc0e30156f149dd60f1ac916603c6423f36
nathan choi <nath49ers@gmail.com>
2024-05-04 17:12:23 -0700

    ToDO

--- src/lib/ai/ai-context.tsx
+++ src/lib/ai/ai-context.tsx
@@ -1,116 +1,124 @@
-'use client'
+"use client";
 
-import { WeightType, AIOutput, EffectType, Question, QuestionInput, STI, Tag, ScoreCalculation, calculateScore, getDefaultScore } from "./questions"
-import { createContext, useContext, useState } from "react"
+import {
+  WeightType,
+  AIOutput,
+  EffectType,
+  Question,
+  QuestionInput,
+  STI,
+  Tag,
+  ScoreCalculation,
+  calculateScore,
+  getDefaultScore,
+} from "./questions";
+import { createContext, useContext, useState } from "react";
 
-import { Question_SexualActivity } from "./questions"
+import { Question_SexualActivity } from "./questions";
 
 type AIContextType = {
-  questionsLeft: number
-  answeredQuestions: Question[]
+  questionsLeft: number;
+  answeredQuestions: Question[];
 
-  resetQuestions: () => Question
-  answerQuestion: (answer: QuestionInput) => Question
-  calculateOutput: () => AIOutput
-  getTopQuestion: () => Question | undefined
-}
+  resetQuesitons: () => Question;
+  answerQuestion: (answer: QuestionInput) => Question;
+  calculateOutput: () => AIOutput;
+  getTopQuestion: () => Question | undefined;
+};
 
 export const AIContext = createContext<AIContextType | null>(null);
 
-export default function AIContextProvider({ children }: { children: React.ReactNode }) {
+export default function AIContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [questionsLeft, setQuestionsLeft] = useState(1);
 
-  const [questionsLeft, setQuestionsLeft] = useState(1)
-
-  const [questionsStack, setQuestionsStack] = useState<Question[]>([new Question_SexualActivity()])
-  const [prunedTags, setPrunedTags] = useState<Tag[]>([])
-  const [answeredQuestions, setAnsweredQuestions] = useState<Question[]>([])
+  const [questionsStack, setQuestionsStack] = useState<Question[]>([
+    new Question_SexualActivity(),
+  ]);
+  const [prunedTags, setPrunedTags] = useState<Tag[]>([]);
+  const [answeredQuestions, setAnsweredQuestions] = useState<Question[]>([]);
 
   const getTopQuestion = (): Question | undefined => {
-    return questionsStack[0]
-  }
+    return questionsStack[0];
+  };
 
   const raise = (msg: string): never => {
-    throw new Error(msg)
-  }
+    throw new Error(msg);
+  };
 
   const answerQuestion = (answer: QuestionInput): Question => {
-    let current = getTopQuestion() ?? raise("Answering without a question!")
-    let effects = current.effects(answer)
-    let newQuestionStack = questionsStack.slice(1)
+    let current = getTopQuestion() ?? raise("Answering without a question!");
+    let effects = current.effects(answer);
+    let newQuestionStack = questionsStack.slice(1);
 
-    setQuestionsLeft(l => l - 1)
+    setQuestionsLeft((l) => l - 1);
 
     for (const effect of effects) {
       if (effect.type === EffectType.AddQuestion) {
-        newQuestionStack = [...effect.questions ?? [], ...newQuestionStack]
-        setQuestionsLeft(l => l + newQuestionStack.length)
-      }
-      else if (effect.type === EffectType.End) {
-        setQuestionsLeft(0)
-      }
-      else if (effect.type === EffectType.PruneTags) {
-        setPrunedTags([...new Set<Tag>([...prunedTags, ...effect.payload ?? []])])
+        newQuestionStack = [...(effect.questions ?? []), ...newQuestionStack];
+        setQuestionsLeft((l) => l + newQuestionStack.length);
+
+        // TODO: prune tags before you add
+      } else if (effect.type === EffectType.End) {
+        setQuestionsLeft(0);
+      } else if (effect.type === EffectType.PruneTags) {
+        setPrunedTags([
+          ...new Set<Tag>([...prunedTags, ...(effect.payload ?? [])]),
+        ]);
       }
     }
 
-    current.selected = answer.id
-    setAnsweredQuestions(prev => [...prev, current])
-    setQuestionsStack(newQuestionStack)
+    current.selected = answer.id;
+    setAnsweredQuestions((prev) => [...prev, current]);
+    setQuestionsStack(newQuestionStack);
 
-    return newQuestionStack[0]
-  }
+    return newQuestionStack[0];
+  };
 
   const calculateOutput = (): AIOutput => {
-    let behavioral: ScoreCalculation = getDefaultScore()
-    let symptomatic: ScoreCalculation = getDefaultScore()
+    let behavioral: ScoreCalculation = getDefaultScore();
+    let symptomatic: ScoreCalculation = getDefaultScore();
 
     // fill with default values
-    let risks: Map<STI, ScoreCalculation> = new Map()
+    let risks: Map<STI, ScoreCalculation> = new Map();
     for (const sti of Object.values(STI)) {
-      risks.set(sti, getDefaultScore())
+      risks.set(sti, getDefaultScore());
     }
 
     for (const question of answeredQuestions) {
       if (!question.selected) continue;
 
-      const selectedAnswer = question.inputOptions
-        .find((option) => option.id === question.selected)
+      const selectedAnswer = question.inputOptions.find(
+        (option) => option.id === question.selected,
+      );
 
       if (!selectedAnswer) {
-        console.log("Skipping question with no selected answer", question)
-        continue
-      };
+        console.log("Skipping question with no selected answer", question);
+        continue;
+      }
 
-      const score = selectedAnswer.value * question.weight
-
+      const score = selectedAnswer.value * question.weight;
 
       if (question.weightType === WeightType.Additive) {
-        if (question.tags.includes(Tag.Behavioral))
-          behavioral.sum += score
-        if (question.tags.includes(Tag.Symptom))
-          symptomatic.sum += score
+        if (question.tags.includes(Tag.Behavioral)) behavioral.sum += score;
+        if (question.tags.includes(Tag.Symptom)) symptomatic.sum += score;
         for (const [sti, risk] of question.riskFactors) {
-          risks.get(sti)!.sum += score * risk
+          risks.get(sti)!.sum += score * risk;
         }
-      }
-
-      else if (question.weightType === WeightType.Multiplicative) {
-        if (question.tags.includes(Tag.Behavioral))
-          behavioral.mul *= score
-        if (question.tags.includes(Tag.Symptom))
-          symptomatic.mul *= score
+      } else if (question.weightType === WeightType.Multiplicative) {
+        if (question.tags.includes(Tag.Behavioral)) behavioral.mul *= score;
+        if (question.tags.includes(Tag.Symptom)) symptomatic.mul *= score;
         for (const [sti, risk] of question.riskFactors) {
-          risks.get(sti)!.mul *= score * risk
+          risks.get(sti)!.mul *= score * risk;
         }
-      }
-
-      else if (question.weightType === WeightType.Exponential) {
-        if (question.tags.includes(Tag.Behavioral))
-          behavioral.exp *= score
-        if (question.tags.includes(Tag.Symptom))
-          symptomatic.exp *= score
+      } else if (question.weightType === WeightType.Exponential) {
+        if (question.tags.includes(Tag.Behavioral)) behavioral.exp *= score;
+        if (question.tags.includes(Tag.Symptom)) symptomatic.exp *= score;
         for (const [sti, risk] of question.riskFactors) {
-          risks.get(sti)!.exp *= score * risk
+          risks.get(sti)!.exp *= score * risk;
         }
       }
     }
@@ -118,39 +126,43 @@ export default function AIContextProvider({ children }: { children: React.ReactN
     const output: AIOutput = {
       behavior: calculateScore(behavioral),
       symptomatic: calculateScore(symptomatic),
-      risks: new Map([...risks].map(([sti, score]) => [sti, calculateScore(score)]))
-    }
+      risks: new Map(
+        [...risks].map(([sti, score]) => [sti, calculateScore(score)]),
+      ),
+    };
 
-    return output
-  }
+    return output;
+  };
 
   const resetQuestions = (): Question => {
-    let start = new Question_SexualActivity()
-    setAnsweredQuestions([])
-    setQuestionsStack([start])
-    setQuestionsLeft(1)
-    return start
-  }
+    let start = new Question_SexualActivity();
+    setAnsweredQuestions([]);
+    setQuestionsStack([start]);
+    setQuestionsLeft(1);
+    return start;
+  };
 
   return (
-    <AIContext.Provider value={{
-      questionsLeft,
-      answeredQuestions,
+    <AIContext.Provider
+      value={{
+        questionsLeft,
+        answeredQuestions,
 
-      resetQuestions,
-      answerQuestion,
-      calculateOutput,
-      getTopQuestion
-    }}>
+        resetQuestions,
+        answerQuestion,
+        calculateOutput,
+        getTopQuestion,
+      }}
+    >
       {children}
     </AIContext.Provider>
-  )
+  );
 }
 
 export function useAIContext() {
-  const context = useContext(AIContext)
+  const context = useContext(AIContext);
   if (!context) {
-    throw new Error('useAIContext must be used within a AIContextProvider')
+    throw new Error("useAIContext must be used within a AIContextProvider");
   }
-  return context
+  return context;
 }
--- src/lib/ai/questions.ts
+++ src/lib/ai/questions.ts
@@ -24,11 +24,11 @@ export enum EndReason {
 }
 
 export type Effect = {
-  type: EffectType,
-  payload?: Tag[],
-  reason?: EndReason,
-  questions?: Question[],
-}
+  type: EffectType;
+  payload?: Tag[];
+  reason?: EndReason;
+  questions?: Question[];
+};
 
 enum InputType {
   FreeText,
@@ -37,10 +37,10 @@ enum InputType {
 }
 
 export type QuestionInput = {
-  id: string,
-  label: string,
-  value: number,
-}
+  id: string;
+  label: string;
+  value: number;
+};
 
 export enum WeightType {
   Additive = "Additive",
@@ -49,109 +49,107 @@ export enum WeightType {
 }
 
 export type ScoreCalculation = {
-  sum: number,
-  mul: number,
-  exp: number,
-}
+  sum: number;
+  mul: number;
+  exp: number;
+};
 
 export const getDefaultScore = (): ScoreCalculation => {
-  return { sum: 0, mul: 1, exp: 1 }
-}
-
+  return { sum: 0, mul: 1, exp: 1 };
+};
 
 export const calculateScore = (scoreCalc: ScoreCalculation): number => {
-  return (scoreCalc.sum * scoreCalc.mul) ** scoreCalc.exp
-}
+  return (scoreCalc.sum * scoreCalc.mul) ** scoreCalc.exp;
+};
 
 export type AIOutput = {
-  risks: Map<STI, number>
-  behavior: number,
-  symptomatic: number,
-}
-
+  risks: Map<STI, number>;
+  behavior: number;
+  symptomatic: number;
+};
 
 export interface Question {
-  readonly label: string,
-  readonly inputType: InputType,
-  readonly inputOptions: QuestionInput[],
+  readonly label: string;
+  readonly inputType: InputType;
+  readonly inputOptions: QuestionInput[];
 
-  selected?: string,
+  selected?: string;
 
-  readonly weight: number,
-  readonly weightType: WeightType,
-  readonly riskFactors: Map<STI, number>,
-  readonly tags: Tag[],
+  readonly weight: number;
+  readonly weightType: WeightType;
+  readonly riskFactors: Map<STI, number>;
+  readonly tags: Tag[];
 
-  effects: (input: QuestionInput) => Array<Effect>,
+  effects: (input: QuestionInput) => Array<Effect>;
 }
 
 const RISK_ALL_STI = (): Map<STI, number> => {
-  let riskFactors = new Map<STI, number>()
+  let riskFactors = new Map<STI, number>();
   for (const sti of Object.values(STI)) {
-    riskFactors.set(sti, 1)
+    riskFactors.set(sti, 1);
   }
-  return riskFactors
-}
+  return riskFactors;
+};
 
 const RISK_SET = (sti: STI, risk: number): Map<STI, number> => {
-  let riskFactors = new Map<STI, number>()
+  let riskFactors = new Map<STI, number>();
   for (const sti of Object.values(STI)) {
-    riskFactors.set(sti, 1)
+    riskFactors.set(sti, 1);
   }
-  riskFactors.set(sti, risk)
-  return riskFactors
-}
+  riskFactors.set(sti, risk);
+  return riskFactors;
+};
 
 const RISK_MINUS = (sti: STI): Map<STI, number> => {
-  return RISK_SET(sti, 0)
-}
+  return RISK_SET(sti, 0);
+};
 
 const RISK_NONE = (): Map<STI, number> => {
-  return new Map()
-}
+  return new Map();
+};
 
 export class Question_SexualActivity implements Question {
-  label = "How many sexual partners have you had in the last 6 months?"
+  label = "How many sexual partners have you had in the last 6 months?";
   // label = "this is a test question for development purposes"
-  inputType = InputType.SelectOne
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "NONE", label: "0", value: 0 },
     { id: "2", label: "1-2", value: 0.75 },
     { id: "3", label: "3-6", value: 1.5 },
     { id: "4", label: "7-15", value: 2.25 },
     { id: "5", label: "16+", value: 3 },
-  ]
-  weight = 1
-  weightType = WeightType.Multiplicative
-  riskFactors = RISK_ALL_STI()
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Multiplicative;
+  riskFactors = RISK_ALL_STI();
+  tags = [Tag.Behavioral];
 
   effects = (input: QuestionInput): Effect[] => {
     if (input.id === "NONE")
-      return [{ type: EffectType.End, payload: [], reason: EndReason.NoSex }]
+      return [{ type: EffectType.End, payload: [], reason: EndReason.NoSex }];
 
-    return [{
-      type: EffectType.AddQuestion,
-      questions: [
-        new Question_UnsafeSexRate(),
-      ],
-    }]
-  }
+    return [
+      {
+        type: EffectType.AddQuestion,
+        questions: [new Question_UnsafeSexRate()],
+      },
+    ];
+  };
 }
 
 class Question_UnsafeSexRate implements Question {
-  label = "How often do you use protection during sex?"
-  inputType = InputType.SelectOne
+  label = "How often do you use protection during sex?";
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "1", label: "Never", value: 1.1 },
     { id: "2", label: "Rarely", value: 1.01 },
     { id: "4", label: "Most of the time", value: 1 },
     { id: "ALWAYS", label: "Always", value: 0.4 },
-  ]
-  weight = 1
-  weightType = WeightType.Exponential
-  riskFactors = RISK_ALL_STI()
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Exponential;
+  riskFactors = RISK_ALL_STI();
+  tags = [Tag.Behavioral];
 
   effects = (input: QuestionInput): Effect[] => {
     let addedQuestions: Question[] = [
@@ -159,108 +157,115 @@ class Question_UnsafeSexRate implements Question {
       new Question_InfectedSemenInEye(),
       new Question_OralSex(),
       new Question_ReproductiveOrgan(),
-    ]
+    ];
 
     if (input.id === "ALWAYS") {
-      let front: Question[] = [new Question_UnprotectedContact()]
-      addedQuestions = front.concat(addedQuestions)
+      let front: Question[] = [new Question_UnprotectedContact()];
+      addedQuestions = front.concat(addedQuestions);
     }
 
-    return [{
-      type: EffectType.AddQuestion,
-      questions: addedQuestions,
-    }]
-  }
+    return [
+      {
+        type: EffectType.AddQuestion,
+        questions: addedQuestions,
+      },
+    ];
+  };
 }
 
 class Question_UnprotectedContact implements Question {
-  label = "Have you had any sort of contact with someone else's genitals without protection?"
-  inputType = InputType.SelectOne
+  label =
+    "Have you had any sort of contact with someone else's genitals without protection?";
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "1", label: "Yes", value: 1 },
     { id: "2", label: "No", value: 0 },
-  ]
-  weight = 1
-  weightType = WeightType.Additive
-  riskFactors = RISK_MINUS(STI.Gonorrhoea)
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Additive;
+  riskFactors = RISK_MINUS(STI.Gonorrhoea);
+  tags = [Tag.Behavioral];
 
-  effects = () => []
+  effects = () => [];
 }
 
 class Question_SharingUnwashedToys implements Question {
-  label = "Have you used shared, unwashed toys?"
-  inputType = InputType.SelectOne
+  label = "Have you used shared, unwashed toys?";
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "1", label: "Yes", value: 1 },
     { id: "2", label: "No", value: 0 },
-  ]
-  weight = 1
-  weightType = WeightType.Additive
-  riskFactors = RISK_ALL_STI()
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Additive;
+  riskFactors = RISK_ALL_STI();
+  tags = [Tag.Behavioral];
 
-  effects = () => []
+  effects = () => [];
 }
 
 class Question_InfectedSemenInEye implements Question {
-  label = "Have you had semen in your eye?"
-  inputType = InputType.SelectOne
+  label = "Have you had semen in your eye?";
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "1", label: "Yes", value: 1 },
     { id: "2", label: "No", value: 0 },
-  ]
-  weight = 1
-  weightType = WeightType.Additive
-  riskFactors = RISK_MINUS(STI.GenitalWarts)
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Additive;
+  riskFactors = RISK_MINUS(STI.GenitalWarts);
+  tags = [Tag.Behavioral];
 
-  effects = () => []
+  effects = () => [];
 }
 
 class Question_OralSex implements Question {
-  label = "Have you had oral sex?"
-  inputType = InputType.SelectOne
+  label = "Have you had oral sex?";
+  inputType = InputType.SelectOne;
   inputOptions = [
     { id: "1", label: "Yes", value: 1 },
     { id: "2", label: "No", value: 0 },
-  ]
-  weight = 1
-  weightType = WeightType.Additive
-  riskFactors = RISK_SET(STI.GenitalWarts, 0.2)
-  tags = [Tag.Behavioral]
+  ];
+  weight = 1;
+  weightType = WeightType.Additive;
+  riskFactors = RISK_SET(STI.GenitalWarts, 0.2);
+  tags = [Tag.Behavioral];
 
-  effects = () => []
+  effects = () => [];
 }
 
 class Question_ReproductiveOrgan implements Question {
-  label = "What reproductive organs do you have?"
-  inputType = InputType.MultiChoice
+  label = "What reproductive organs do you have?";
+  inputType = InputType.MultiChoice;
   inputOptions = [
     { id: "MALE", label: "Male reproductive organs", value: 1 },
     { id: "FEMALE", label: "Female reproductive organs", value: 1 },
     { id: "NONE", label: "Other", value: 1 },
-  ]
-  weight = 0
-  weightType = WeightType.Additive
-  riskFactors = RISK_NONE()
-  tags = [Tag.Behavioral]
+  ];
+  weight = 0;
+  weightType = WeightType.Additive;
+  riskFactors = RISK_NONE();
+  tags = [Tag.Behavioral];
 
   effects = (input: QuestionInput): Effect[] => {
     if (input.id === "MALE") {
-      return [{
-        type: EffectType.PruneTags,
-        payload: [Tag.Female]
-      }]
+      return [
+        {
+          type: EffectType.PruneTags,
+          payload: [Tag.Female],
+        },
+      ];
     } else if (input.id === "FEMALE") {
-      return [{
-        type: EffectType.PruneTags,
-        payload: [Tag.Female]
-      }]
+      return [
+        {
+          type: EffectType.PruneTags,
+          payload: [Tag.Male],
+        },
+      ];
     } else {
-      return []
+      return [];
     }
-  }
+  };
 }
 
 // // Question that isn't scored just used to skip a category of questions
@@ -283,7 +288,7 @@ class Question_ReproductiveOrgan implements Question {
 //         questions: [
 //           new Question_PainlessGenitalSores(),
 //           new Question_
-//           new 
+//           new
 //         ],
 //       }]
 //     }
