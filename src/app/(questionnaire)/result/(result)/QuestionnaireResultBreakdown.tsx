cc29292570255034de142f35a36246acbfc1d5f0
nathan choi <nath49ers@gmail.com>
2024-07-10 15:06:37 -0700

    minor changes

--- src/app/(questionnaire)/result/(result)/QuestionnaireResultBreakdown.tsx
+++ src/app/(questionnaire)/result/(result)/QuestionnaireResultBreakdown.tsx
@@ -32,7 +32,7 @@ export default function QuestionnaireResultBreakdown() {
               .map(risk => [risk[0], Math.ceil(risk[1])])
 
             return (
-              <div key={i} className="flex flex-col gap-[10px] rounded-lg border border-secondary/0 p-5 transition-all hover:scale-[1.01] hover:border-secondary/5 hover:bg-stone-200/30">
+              riskFactors.length > 0 && <div key={i} className="flex flex-col gap-[10px] rounded-lg border border-secondary/0 p-5 transition-all hover:scale-[1.01] hover:border-secondary/5 hover:bg-stone-200/30">
                 <Bar className="mb-2" percent={percent} />
 
                 <h2 className="text-sm font-medium">{question.label}</h2>
@@ -55,8 +55,9 @@ export default function QuestionnaireResultBreakdown() {
                         riskFactors.length > 0 && <span className="mt-5 flex gap-[10px]">
                           {
                             riskFactors.map((risk, i) => (
-                              <span key={i} className="flex cursor-default select-none items-center justify-center gap-1 rounded-full bg-primary px-2 py-1 text-[12px] text-white">
-                                <img src={iconLinks[risk[1] - 1]} className="text-color" width={15} height={15} alt={""} />
+                              <span key={i} className="flex cursor-default select-none items-center justify-center rounded-full bg-primary px-2 py-1 text-[12px] text-white shadow-sm">
+                                <img src={iconLinks[risk[1] - 1]} className="text-color mr-1" width={20} height={20} alt={""} />
+                                {/* <span className="ml-[5px] mr-[8px] text-[10px]">{"!".repeat(risk[1])}</span> */}
                                 {risk[0]}
                               </span>
                             ))
