114c8c8f1701d4adfb5b7d9e1f1640f7e0c4fa80
b0kch01 <nath49ers@gmail.com>
2024-07-20 16:43:56 -0700

    fix build error for now before routing overhaul

--- src/app/(questionnaire)/result/page.tsx
+++ src/app/(questionnaire)/result/page.tsx
@@ -3,13 +3,16 @@
 import QuestionnaireResults from "./(result)/QuestionnaireResults";
 import { FlowerContextProvider } from "@/lib/contexts/FlowerContext";
 import AIContextProvider from "@/lib/ai/ai-context";
+import { Suspense } from "react";
 
 export default function ResultPage() {
   return (
     <div className="mt-[105px] flex flex-grow flex-col">
       <AIContextProvider>
         <FlowerContextProvider>
-          <QuestionnaireResults />
+          <Suspense>
+            <QuestionnaireResults />
+          </Suspense>
         </FlowerContextProvider>
       </AIContextProvider>
     </div>
